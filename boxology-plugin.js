ad99ca6e12958b68b55fa47be589fdbeed4d9974
Mahsa-for <mforghanitehrani@gmail.com>
2025-02-14 16:31:33 +0100

    Update boxology-plugin.js

--- boxology-plugin.js
+++ boxology-plugin.js
@@ -1,35 +1,26 @@
-Draw.loadPlugin(function (ui) {
-    console.log("Boxology Plugin Loaded");
+Draw.loadPlugin(function(ui) {
+    // Define the Boxology stencil library
+    var boxologyStencil = `
+    <mxlibrary>
+        [
+            {"title":"Symbol","data":"<shape name='symbol' aspect='fixed'><rect x='10' y='10' width='80' height='40' strokeWidth='2'/></shape>"},
+            {"title":"Generate: Train","data":"<shape name='generate_train' aspect='fixed'><ellipse x='10' y='10' width='80' height='40' strokeWidth='2'/></shape>"},
+            {"title":"Model","data":"<shape name='model' aspect='fixed'><polygon x='10' y='10' width='80' height='40' strokeWidth='2' points='0 0, 80 20, 80 40, 0 40'/></shape>"},
+            {"title":"Transform","data":"<shape name='transform' aspect='fixed'><ellipse x='10' y='10' width='80' height='40' strokeWidth='2'/></shape>"},
+            {"title":"Actor","data":"<shape name='actor' aspect='fixed'><polygon x='10' y='10' width='80' height='40' strokeWidth='2' points='40 0, 80 40, 0 40'/></shape>"}
+        ]
+    </mxlibrary>`;
 
-    // Get the Graph
-    let graph = ui.editor.graph;
-    let parent = graph.getDefaultParent();
+    // Parse and add custom stencil palette
+    mxResources.parse('boxology=Boxology Patterns');
+    Sidebar.prototype.addBoxologyPalette = function() {
+        var s = boxologyStencil;
+        this.addStencilPalette('boxology', 'Boxology', s);
+    };
 
-    graph.getModel().beginUpdate();
-    try {
-        // Define Nodes
-        let dataNode = graph.insertVertex(
-            parent, null, "Data", 50, 50, 100, 50,
-            "shape=rectangle;fillColor=white;strokeColor=green;"
-        );
+    // Register the Boxology Palette in the sidebar
+    ui.sidebar.addBoxologyPalette();
 
-        let mlTrainingNode = graph.insertVertex(
-            parent, null, "ML_Training", 200, 50, 120, 50,
-            "shape=rectangle;fillColor=white;strokeColor=blue2;"
-        );
-
-        let mlModelNode = graph.insertVertex(
-            parent, null, "ML_Model", 350, 50, 120, 50,
-            "shape=hexagon;fillColor=white;strokeColor=blue;"
-        );
-
-        // Define Edges
-        graph.insertEdge(parent, null, "", dataNode, mlTrainingNode);
-        graph.insertEdge(parent, null, "", mlTrainingNode, mlModelNode);
-
-    } finally {
-        graph.getModel().endUpdate();
-    }
-
-    console.log("Boxology Graph Loaded");
+    // Display confirmation in console
+    console.log('Boxology plugin loaded successfully!');
 });
